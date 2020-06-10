886b1187b6c999c6dd6ed3c79bb14974a2a3b387
Ishan Guru <ishanguru@ishans-mbp-2.home>
2020-06-10 18:18:12 -0400

    Added endpoints for task handling

--- accountability-server/src/routes/routes.go
+++ accountability-server/src/routes/routes.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"github.com/gorilla/mux"
 	"../services"
+	"../taskhandler"
 )
 
 
@@ -18,6 +19,11 @@ func Handlers() *mux.Router {
 	r.HandleFunc("/auth/login", auth.LoginHandler).Methods("POST")
 	r.HandleFunc("/auth/logout", auth.LogoutHandler).Methods("POST")
 
+	// task management services
+	r.HandleFunc("/tasks/createTask", taskhandler.CreateTask).Methods("POST")
+	r.HandleFunc("/tasks/updateTask", taskhandler.UpdateTask).Methods("POST")
+	r.HandleFunc("/tasks/removeTask", taskhandler.RemoveTask).Methods("POST")
+	r.HandleFunc("/tasks/getTasks", taskhandler.GetTasks).Methods("POST")
 
 	return r
 }
