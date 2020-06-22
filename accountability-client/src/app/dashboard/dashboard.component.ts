f2fbe9d654d6820d234888ef970d073b742d471e
Ishan Guru <ig2333@columbia.edu>
2020-06-22 16:05:26 -0400

    Add basic routing to accoutability dashboard

--- accountability-client/src/app/dashboard/dashboard.component.ts
+++ accountability-client/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  
+  username: String;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      this.username = params['username'];
+      //TODO guruis - If username not provided here, throw error
+    });
   }
 
 }
